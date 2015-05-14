3b4f4b83d0c6bdb7b0032c197a7b330943813069
Cody Nguyen <cnguyen235@yahoo.com>
2015-05-13 21:04:09 -0700

    test

--- RPG final/src/Character.java
+++ RPG final/src/Character.java
@@ -4,8 +4,27 @@
 
 /**
  * @author Cody
- *
  */
-public interface Character {
+public interface Character
+{
 
+	public String getName();
+
+	public String setName();
+
+	public int getHP();
+
+	public int setHP();
+
+	public int addHP(int hp);
+
+	public int subHP(int hp);
+
+	public int getMP();
+
+	public int setMP();
+
+	public int addMP(int mp);
+
+	public int subMP(int mp);
 }
