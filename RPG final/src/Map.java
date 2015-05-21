f9c7fcf73319e409adf4d4c2e806af1085d1edbb
Cody Nguyen <cnguyen235@yahoo.com>
2015-05-21 12:09:25 -0700

    added map and dungeon stuff

--- RPG final/src/Map.java
+++ RPG final/src/Map.java
@@ -11,6 +11,34 @@ public class Map
 		Room current;
 		FileInput input = new FileInput("dungeon");
 		Room[][] map = new Room[6][6];
+		for (int a = 0; a < 6; a++)
+		{
+			for (int b = 0; b < 6; b++)
+			{
+				if (input.readToken() == "null")
+				{
+					map[a][b] = null;
+				}
+				else
+				{
+					map[a][b] = new Room(input.readLine(), input.readInt());
+				}
+			}
+		}
+		
+		current = map[input.readInt()][input.readInt()];
+		
+		for (int a = 0; a < 6; a++)
+		{
+			for (int b = 0; b < 6; b++)
+			{
+				if (map[a][b] != null)
+				{
+					
+				}
+				
+			}
+		}
 		
 		
 	}
